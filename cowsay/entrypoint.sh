3315d9120a7ac0757791d604b86636e51b60b919
AnatoliyYud <trouble93@yandex.ru>
2022-08-09 12:41:49 +0300

    modify dickerfile and entrypoint.sh

--- cowsay/entrypoint.sh
+++ cowsay/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
-/usr/games/fortune | /usr/games/cowsay
-else
-/usr/games/cowsay "$@"
+    /usr/games/fortune | /usr/games/cowsay
+  else
+    /usr/games/cowsay "$@"
 fi
